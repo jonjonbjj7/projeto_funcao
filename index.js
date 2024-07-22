14f346d429f09b32c5fb3deda5509b6299b3a891
jonjonbjj7 <jonjonlopes00@gmail.com>
2024-07-22 13:17:03 -0300

    projeto_funcao

---
+++ index.js
@@ -0,0 +1,86 @@
+const saldo = "O saldo da rankeada é "
+
+let vitoria = subtrair(10, 5)
+
+console.log(saldo + vitoria)
+
+function subtrair(ferroOne, ferroTwo){
+    let subtração = ferroOne - ferroTwo
+    return subtração
+};
+
+let win = subtrair(20, 3)
+console.log(saldo + win)
+
+function subtrair(bronzeOne, bronzeTwo){
+        let subtração = bronzeOne - bronzeTwo
+        return subtração};
+
+
+let winOne = subtrair(50, 28)
+
+console.log(saldo + winOne)
+
+function subtrair(prataOne, prataTwo){
+        let subtração = prataOne - prataTwo
+        return subtração
+}
+
+let winTwo = subtrair(80, 29) 
+
+console.log(saldo + winTwo)
+ function subtrair(ouroOne, ouroTwo){
+        let subtração = ouroOne - ouroTwo
+        return subtração
+    };
+
+let winThree = subtrair(89,4)
+console.log(saldo + winThree)
+
+function subtrair(diamanteOne, diamanteTwon){
+        let subtração = diamanteOne - diamanteTwon
+        return subtração
+};
+
+let winFour = subtrair(100,9)
+console.log(saldo + winFour)
+function subtrair(lendarioOne,lendarioTwo){
+        let subtração = lendarioOne - lendarioTwo
+        return subtração
+};
+
+let winFive = subtrair(101,1)
+console.log(saldo + winFive)
+
+function subtrair(imortalOne,imortalTwo){
+        let subtração = imortalOne - imortalTwo
+        return subtração
+};
+
+let imortalMaisForte = true
+let imortalMaiorLevel = true
+let podeAplicar = imortalMaisForte && imortalMaiorLevel
+
+console.log(podeAplicar)
+
+for (let contadorDoMaisFracoAoMaisForte = 10; contadorDoMaisFracoAoMaisForte < 101; contadorDoMaisFracoAoMaisForte++){
+        console.log(contadorDoMaisFracoAoMaisForte)
+}
+
+let heroi = 'imortal'
+
+switch (heroi){
+        case 'ferro':
+        console.log('escolher héroi')
+        break
+        
+        case 'imortal':
+        console.log('escolher héroi')
+        break
+       
+        default:
+        console.log('escolher herói aleatório')
+        break
+}
+
+console.log("O Herói tem de saldo de 100 está no nível de imortal")
